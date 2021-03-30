f467b80bec3d203ed0a0eb07745e54084051efdd
Gautham Chengappa <gautham.hc@gmail.com>
2021-03-30 16:25:35 +0530

    about page completing

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+function myFunction1() {
+  var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("myBtn1");
+
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    btnText1.innerHTML = "Read more";
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none";
+    btnText1.innerHTML = "Read less";
+    moreText1.style.display = "inline";
+  }
+}
